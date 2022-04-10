28b767bec3013380f39f9e05c9168e9581d52849
Zahra Ghavasieh <judgyknowitall@hotmail.com>
2022-04-09 22:31:07 -0600

    Added OrenNayar

--- src/shaders/lightPass.fs.glsl
+++ src/shaders/lightPass.fs.glsl
@@ -1,5 +1,7 @@
 #version 410 core
 
+#define PI 3.1415926538
+
 out vec4 FragColor;
 
 in vec2 TexCoords;
@@ -19,21 +21,13 @@ uniform vec4 bg = vec4(0.2, 0.1, 0, 1);         // the background colour
 uniform vec3 ambient = vec3(0.1, 0.07, 0.03);   // Ambient Light
 
 
-void main()
-{
 
-    // check if we're a background pixel ASAP to save on computing
-    float depth = texture(gDepth, TexCoords).x;
-    if (depth == 1) {
-        FragColor = bg;
-        return;
-    }
 
-    // retrieve data from gbuffer
-    vec3 FragPos = texture(gPosition, TexCoords).xyz;
-    vec3 Normal = texture(gNormal, TexCoords).xyz;
-    vec3 Diffuse = texture(gColor, TexCoords).rgb;
-    float Specular = texture(gColor, TexCoords).a;
+/////////////////////////////////////////////////////////////////////
+// Phong Shading
+/////////////////////////////////////////////////////////////////////
+
+void phong(vec3 FragPos, vec3 Normal, vec3 Diffuse, float Specular) {
 
     float specular_power = 81 * Specular * Specular;
 
@@ -50,7 +44,59 @@ void main()
         vec3 specular = pow(max(dot(R, FragPos), 0.0), specular_power) * Specular * vec3(1, 1, 1);
         FragColor = vec4(ambient + diffuse + specular, 1.0);
     }
+}
 
+
+/////////////////////////////////////////////////////////////////////
+// Oren-Nayar
+/////////////////////////////////////////////////////////////////////
+// Yasuhiro Fujii's "improvement" is lightweight yet a pretty good match for
+//  "full" Oren-Nayar. See https://mimosa-pudica.net/improved-oren-nayar.html
+
+void orennayar(vec3 FragPos, vec3 Normal, vec3 Diffuse, float Specular) {
+
+    float NdL = max(dot(Normal, L), 0);
+    float NdV = max(dot(Normal, FragPos), 0);
+
+    float s = max(dot(L, V), 0) - NdL * NdV;
+    float t = 1;
+    if (s > 0)
+        t = max(NdL, NdV);
+
+    float invA = 1. + (.5 - (2. / (PI * 3.))) * Specular;
+    float B = Specular / invA;
+
+    float factor = (1. / invA + B * s / t) * NdL;
+    if ((factor > 1) && (NdL < 0.01))		// smooth out a math singularity
+        FragColor = vec4(Diffuse * NdL, 1.0);
+    else
+        FragColor = vec4(Diffuse * factor, 1.0);
+}
+
+
+
+/////////////////////////////////////////////////////////////////////
+// MAIN
+/////////////////////////////////////////////////////////////////////
+
+void main()
+{
+
+    // check if we're a background pixel ASAP to save on computing
+    float depth = texture(gDepth, TexCoords).x;
+    if (depth == 1) {
+        FragColor = bg;
+        return;
+    }
+
+    // retrieve data from gbuffer
+    vec3 FragPos = texture(gPosition, TexCoords).xyz;
+    vec3 Normal = texture(gNormal, TexCoords).xyz;
+    vec3 Diffuse = texture(gColor, TexCoords).rgb;
+    float Specular = texture(gColor, TexCoords).a;
+
+    //phong(FragPos, Normal, Diffuse, Specular);
+    orennayar(FragPos, Normal, Diffuse, Specular);
 
     // TESTS
     //FragColor = vec4( Diffuse, 1.0 );	// albedo
